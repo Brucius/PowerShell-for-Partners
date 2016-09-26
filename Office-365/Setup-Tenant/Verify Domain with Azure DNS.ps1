82c43ca20c0de1dacf92bcc25ef04b5dd8fcb73a
AKonCloud <akorenkov.ak@gmail.com>
2016-09-26 22:37:05 +1000

    DNS

---
+++ Office-365/Setup-Tenant/Verify Domain with Azure DNS.ps1
@@ -0,0 +1,32 @@
+﻿## Authenticate as partner
+$PacCreds = Get-Credential
+
+## PAC Login
+Connect-MsolService -Credential $PacCreds
+
+## Find the company tenant
+$TenantId = (Get-MsolPartnerContract -All | Select-Object -Property DefaultDomainName, Name, TenantId | Sort-Object -Property Name | Out-GridView -Title "Find the customer..." -PassThru).TenantId
+
+## Azure Login, must be after finding TenantId
+Login-AzureRmAccount -Credential $PacCreds -TenantId $TenantId
+
+## Find the Azure SubscriptionId
+## Add count and if for SubscriptionId
+$SubId = (Get-AzureRmSubscription -TenantId $TenanId| Out-GridView -Title "Select the subscription..." -PassThru).SubscriptionId
+
+## Input domain name, exclude http and www e.g. microsoft.com
+Write-Host "`n`tEnter the domain name: " -ForegroundColor Cyan -nonewline;
+$choice = Read-Host
+$DomainName = $choice
+
+## Check if DNS Resource Group exists. If not, create it in Australia East (Sydney)
+## Add check
+New-AzureRmResourceGroup -Name "DNS" -location "Australia East"
+
+## Check is DNS Zone Already exists. If not, create it.
+## Add check
+New-AzureRmDnsZone -Name $DomainName -ResourceGroupName "DNS"
+
+## Output nameservers
+$Records = (Get-AzureRmDnsRecordSet -ZoneName $DomainName -ResourceGroupName "DNS" -RecordType NS).Records
+$Records
