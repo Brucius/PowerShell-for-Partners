7b9a2482d460dd927067c29d913e9cc6cc4c7043
AKonCloud <akorenkov.ak@gmail.com>
2016-09-26 21:59:44 +1000

    Up

--- Office-365/Create new user.ps1
+++ Office-365/Create new user.ps1
@@ -5,7 +5,7 @@ $PacCreds = Get-Credential
 Connect-MsolService -Credential $PacCreds
 
 ## Find the company tenant
-$TenantId = (Get-MsolPartnerContract -All | Out-GridView -Title "Find the customer..." -PassThru).TenantId
+$TenantId = (Get-MsolPartnerContract -All | Select-Object -Property DefaultDomainName, Name, TenantId | Sort-Object -Property Name | Out-GridView -Title "Find the customer..." -PassThru).TenantId
 
 ## Choose the domain
 $DomainCount = (Get-MsolDomain -TenantId $TenantId | ? {$_.Name -notmatch 'onmicrosoft.com'}).count
@@ -26,7 +26,7 @@ $LastName = $choice
 
 ## Generate UPN and DisplayName
 $UPN = "{0}{1}{2}{3}{4}" -f $firstname, '.', $lastname, '@', $domain
-$DisplayName =  "{0}{1}" -f $firstname, $lastname
+$DisplayName =  "{0}{1}{2}" -f $firstname, ' ', $lastname
 
 ## Where is the user
 ## Write selection menu for countries
@@ -34,9 +34,26 @@ $DisplayName =  "{0}{1}" -f $firstname, $lastname
 ## Choose the licenses
 $Licenses = (Get-MsolAccountSku -TenantId $TenantId | Out-GridView -PassThru).AccountSkuId
 
-## Set country
+## Set country, default uses tenant country
 $Country = (Get-MsolCompanyInformation -TenantId $TenantId).CountryLetterCode
-$Country
 
-## Create new user
-New-MsolUser  -Tenant $TenantId -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UPN -UsageLocation $Country -LicenseAssignment $Licenses
+## Create new user and set Object ID variable
+New-MsolUser  -TenantId $TenantId -DisplayName $DisplayName -FirstName $FirstName -LastName $LastName -UserPrincipalName $UPN -UsageLocation $Country -LicenseAssignment $Licenses
+$UserId = (Get-MsolUser -TenantId $TenantId -UserPrincipalName $UPN).ObjectId
+
+## Set alternative email address
+Set-MsolUser -Tenant $TenantId -ObjectId $UserId -AlternateEmailAddresses "a@A.com" -MobilePhone $Cellphone
+
+## Show user details
+Write-Host "`n`tFirst name: " -ForegroundColor Cyan -nonewline; $FirstName
+Write-Host "`n`tLast name: " -ForegroundColor Cyan -nonewline; $LastName
+Write-Host "`n`tDisplay name: " -ForegroundColor Cyan -nonewline; $DisplayName
+Write-Host "`n`tUser location: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tUPN: " -ForegroundColor Cyan -nonewline; $UPN
+Write-Host "`n`tAlternate Email: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tCellphone: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tLicenses: " -ForegroundColor Cyan -nonewline; $Licenses
+Write-Host "`n`tTemp Password: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tPSTN Pin: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tPSTN ID: " -ForegroundColor Cyan -nonewline; $Country
+Write-Host "`n`tPSTN Number: " -ForegroundColor Cyan -NoNewline; $Country
---
+++ Office-365/Open Admin Center.ps1
@@ -0,0 +1,18 @@
+﻿## Authenticate as partner
+$PacCreds = Get-Credential
+
+## PAC Login
+Connect-MsolService -Credential $PacCreds
+
+## Find the company tenant
+$TenantId = (Get-MsolPartnerContract -All | Select-Object -Property DefaultDomainName, Name, TenantId | Sort-Object -Property Name | Out-GridView -Title "Find the customer..." -PassThru).TenantId
+$DefaultDomain = (Get-MsolDomain -TenantId $TenantId | ? {$_.Name -match '.onmicrosoft.com'}).name
+
+## Choose Admin Center
+## Menu to choose Admin Center
+
+## Open customer O365 Admin Center
+Start-Process -FilePath "https://portal.office.com/Partner/BeginClientSession.aspx?CTID=$TenantId&CSDEST=o365admincenter"
+
+## Open customer Azure Portal
+Start-Process -FilePath "https://portal.azure.com/$DefaultDomain"
