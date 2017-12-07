41f084a9fc0a6bbe228bcff16dab82f1be55eb3d
Broozius <Brucetheflame@outlook.com>
2017-12-07 21:57:28 +0800

    Update CopyManagedDisktoStorageAccount.ps1
    Putting the status of the copy job

--- Azure/CopyManagedDisktoStorageAccount.ps1
+++ Azure/CopyManagedDisktoStorageAccount.ps1
@@ -2,4 +2,18 @@ $sas = Grant-AzureRmDiskAccess -ResourceGroupName $RG -DiskName $diskname -Durat
 
 $destContext = New-AzureStorageContext –StorageAccountName $SAName -StorageAccountKey $SAKey 
 
-Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $SAContainer -DestContext $destContext -DestBlob '$diskname.vhd'
+$blob1 = Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $SAContainer -DestContext $destContext -DestBlob '$diskname.vhd'
+
+### Retrieve the current status of the copy operation ###
+$status = $blob1 | Get-AzureStorageBlobCopyState 
+ 
+### Print out status ### 
+$status 
+ 
+### Loop until complete ###                                    
+While($status.Status -eq "Pending"){
+  $status = $blob1 | Get-AzureStorageBlobCopyState 
+  Start-Sleep 10
+  ### Print out status ###
+  $status
+} 
