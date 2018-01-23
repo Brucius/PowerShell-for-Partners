2125a11fd66870f0bd226103b5adb9c09b16a7ba
Broozius <Brucetheflame@outlook.com>
2018-01-23 18:26:59 +0800

    Create AzureExpressRouteSetUDR.ps1

---
+++ Azure/AzureExpressRouteSetUDR.ps1
@@ -0,0 +1,15 @@
+Add-AzureRmAccount
+
+# Get information on the virtual network
+$vnet = Get-AzureRmVirtualNetwork -ResourceGroupName "ResourceGroup" -Name "VNet"
+
+# Create a new route table and define the route
+$RouteTable = New-AzureRmRouteTable -Name "RouteTableName" -ResourceGroupName "ResourceGroup" -Location "Southeast Asia"
+
+Add-AzureRmRouteConfig -Name "KMSActivate" -AddressPrefix 23.102.135.246/32 -NextHopType Internet -RouteTable $RouteTable
+Set-AzureRmRouteTable -RouteTable $RouteTable
+
+# Apply the route defined above to the subnet
+$forcedTunnelVNet = $vnet.Subnets | ? Name -eq "Subnet-Name"
+$forcedTunnelVNet.RouteTable = $RouteTable
+Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
