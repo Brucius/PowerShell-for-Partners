83cccfa44cf7089d55a8142f36e66dc179b02096
Broozius <Brucetheflame@outlook.com>
2018-01-24 13:43:06 +0800

    Update AzureExpressRouteSetUDR.ps1

--- Azure/AzureExpressRouteSetUDR.ps1
+++ Azure/AzureExpressRouteSetUDR.ps1
@@ -1,17 +1,18 @@
-#For Further Updates on IP Changes https://blogs.technet.microsoft.com/jpaztech/2016/05/16/azure-vm-may-fail-to-activate-over-expressroute/
+#For Further Updates on IP Changes of Azure KMS Service please visit
+#https://blogs.technet.microsoft.com/jpaztech/2016/05/16/azure-vm-may-fail-to-activate-over-expressroute/
 
 Add-AzureRmAccount
 
-# Get information on the virtual network
+#Get information on the virtual network
 $vnet = Get-AzureRmVirtualNetwork -ResourceGroupName "ResourceGroup" -Name "VNet"
 
-# Create a new route table and define the route
+#Create a new route table and define the route
 $RouteTable = New-AzureRmRouteTable -Name "RouteTableName" -ResourceGroupName "ResourceGroup" -Location "Southeast Asia"
 
 Add-AzureRmRouteConfig -Name "KMSActivate" -AddressPrefix 23.102.135.246/32 -NextHopType Internet -RouteTable $RouteTable
 Set-AzureRmRouteTable -RouteTable $RouteTable
 
-# Apply the route defined above to the subnet
+#Apply the route defined above to the subnet
 $forcedTunnelVNet = $vnet.Subnets | ? Name -eq "Subnet-Name"
 $forcedTunnelVNet.RouteTable = $RouteTable
 Set-AzureRmVirtualNetwork -VirtualNetwork $vnet
