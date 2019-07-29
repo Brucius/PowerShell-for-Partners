440a4f8ecf26c6f736a3b082a80571c3ce264f13
root <root@RHP-OBSINOMNLT.localdomain>
2019-07-29 11:38:57 +0800

    refactor statusloopcopy managed disk

--- AzureRmTemplates/StatusLoopCopyManagedDisktoStorageAccount.ps1
+++ AzureRmTemplates/StatusLoopCopyManagedDisktoStorageAccount.ps1
@@ -1,11 +1,24 @@
+#This command sets the execution policy to bypass for only the current PowerShell session.
+#After the window is closed, the next PowerShell session will open running with the default execution policy. 
+#“Bypass” means nothing is blocked and no warnings, prompts, or messages will be displayed.
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+##Define variables
+$sourceRG = "sourceRgName"
+$sourceDisk = "sourceDiskName"
+$targetStorageAccount = "targetStorageAccountName"
+$targetStorageAccountKey = "targetStorageAccountKey"
+$targetStorageAccountContainer = "targetStorageAccountContainerName"
+$targetDisk = "targetDiskName.vhd"
+
 ##Login
-Add-AzureRmAccount 
+Login-AzureRmAccount
 
-$sas = Grant-AzureRmDiskAccess -ResourceGroupName "$RG" -DiskName "$diskname" -DurationInSecond 3600 -Access Read 
+$sas = Grant-AzureRmDiskAccess -ResourceGroupName $sourceRG -DiskName $sourceDisk -DurationInSecond 3600 -Access Read 
 
-$destContext = New-AzureStorageContext –StorageAccountName "$SAName" -StorageAccountKey "$SAKey" 
+$destContext = New-AzureStorageContext –StorageAccountName $targetStorageAccount -StorageAccountKey $targetStorageAccountKey 
 
-$blob1 = Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer "$SAContainer" -DestContext $destContext -DestBlob '$diskname.vhd'
+$blob1 = Start-AzureStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $targetStorageAccountContainer -DestContext $destContext -DestBlob $targetDisk
 
 ### Retrieve the current status of the copy operation ###
 $status = $blob1 | Get-AzureStorageBlobCopyState 
